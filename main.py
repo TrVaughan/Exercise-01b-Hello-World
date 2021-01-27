d6d9eb58be8e7e6e9291e75da73b8e715d8d1ff3
TrVaughan <trvaugha@iu.edu>
2021-01-27 13:24:09 -0500

    Completed Excersize Hellow World!

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 import sys
 assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
+print("Hello, World!")
+print("I would like to get to know you.")
+n = input("What is your name? ")
+if n == "Trinity":
+    print("What do I need to do to get an A? ")
+elif n == "Jared":
+    print("I've got a bad feeling about you")
+else:
+    print("I'm glad to meet you, {}".format(n))
